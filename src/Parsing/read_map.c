209dc575ee3bf10f6d4753f21e6f1d69c87ce83c
Alessio Benincasa <albeninc@paul-f3Ar1s2.clusters.42paris.fr>
2024-03-08 17:45:29 +0100

    pushed the parse scene added even more stuff

--- src/Parsing/read_map.c
+++ src/Parsing/read_map.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: albeninc <albeninc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/08 16:02:57 by albeninc          #+#    #+#             */
-/*   Updated: 2024/03/08 17:20:03 by albeninc         ###   ########.fr       */
+/*   Created: 2024/03/08 17:44:50 by albeninc          #+#    #+#             */
+/*   Updated: 2024/03/08 17:44:53 by albeninc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 void	parse_scene(char *filename)
 {
-	FILE		*file;
-	char		*line;
-	int			fd;
-	char		identifier[3];
-	t_ambient	ambient;
-	t_camera	camera;
-	t_light		light;
-	t_sphere	sphere;
-	t_plane		plane;
-	t_cylinder	cylinder;
-	char		**words;
+	FILE *file;
+	char *line;
+	int fd;
+	char identifier[3];
+	t_ambient ambient;
+	t_camera camera;
+	t_light light;
+	t_sphere sphere;
+	t_plane plane;
+	t_cylinder cylinder;
+	char **words;
 
 	file = open(filename, "r");
 	if (fd == -1)
@@ -39,24 +39,83 @@ void	parse_scene(char *filename)
 		{
 			if (ft_strcmp(words[0], "A") == 0)
 			{
+				if (words_count(words) >= 4)
+				{
+					ambient.ratio = ft_atof(words[1]);
+					ambient.color.r = ft_atoi(words[2]);
+					ambient.color.g = ft_atoi(words[3]);
+					ambient.color.b = ft_atoi(words[4]);
+				}
 			}
 			else if (ft_strcmp(words[0], "C") == 0)
 			{
+				if (words_count(words) >= 8)
+				{
+					camera.origin.x = ft_atof(words[1]);
+					camera.origin.y = ft_atof(words[2]);
+					camera.origin.z = ft_atof(words[3]);
+					camera.direction.x = ft_atof(words[4]);
+					camera.direction.y = ft_atof(words[5]);
+					camera.direction.z = ft_atof(words[6]);
+					camera.fov = ft_atoi(words[7]);
+				}
 			}
 			else if (ft_strcmp(words[0], "L") == 0)
 			{
+				if (words_count(words) >= 7)
+				{
+					light.position.x = atof(words[1]);
+					light.position.y = atof(words[2]);
+					light.position.z = atof(words[3]);
+					light.intensity = atof(words[4]);
+					light.color.r = atoi(words[5]);
+					light.color.g = atoi(words[6]);
+					light.color.b = atoi(words[7]);
+				}
 			}
-			else if (ft_strcmp(words[0], "sp") == 0)
+		}
+		else if (ft_strcmp(words[0], "sp") == 0)
+			if (words_count(words) >= 6)
+
 			{
+				sphere.center.x = atof(words[1]);
+				sphere.center.y = atof(words[2]);
+				sphere.center.z = atof(words[3]);
+				sphere.diameter = atof(words[4]);
+				sphere.color.r = atoi(words[5]);
+				sphere.color.g = atoi(words[6]);
+				sphere.color.b = atoi(words[7]);
 			}
 			else if (ft_strcmp(words[0], "pl") == 0)
 			{
+				if (words_count(words) >= 9)
+
+				{
+					plane.point.x = atof(words[1]);
+					plane.point.y = atof(words[2]);
+					plane.point.z = atof(words[3]);
+					plane.normal.x = atof(words[4]);
+					plane.normal.y = atof(words[5]);
+					plane.normal.z = atof(words[6]);
+					plane.color.r = atoi(words[7]);
+					plane.color.g = atoi(words[8]);
+					plane.color.b = atoi(words[9]);
+				}
 			}
 			else if (ft_strcmp(words[0], "cy") == 0)
 			{
+				if (words_count(words) >= 11)
+
+				{
+					cylinder.position.x = atof(words[1]);
+					cylinder.position.y = atof(words[2]);
+					cylinder.position.z = atof(words[3]);
+					cylinder.direction.x = atof(words[4]);
+					cylinder.direction.y = atof(words[5]);
+					cylinder.direction.z = atof(words[6]);
+
+					free_double_array(words);
+				}
 			}
-			free_double_array(words);
-		}
+		fclose(file);
 	}
-	fclose(file);
-}
