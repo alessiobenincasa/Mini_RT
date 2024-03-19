e175782c6de8205b907b171c95a83b481b264dcc
Alessio Benincasa <albeninc@paul-f4Br4s3.clusters.42paris.fr>
2024-03-19 04:07:45 +0100

    tt

--- src/Engine/main.c
+++ src/Engine/main.c
@@ -6,114 +6,12 @@
 /*   By: albeninc <albeninc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 16:24:32 by albeninc          #+#    #+#             */
-/*   Updated: 2024/03/19 03:58:03 by albeninc         ###   ########.fr       */
+/*   Updated: 2024/03/19 04:06:54 by albeninc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
 
-t_sphere g_sphere;
-t_plane g_plane;
-t_light g_light;
-t_cylinder g_cylinder;
- /*
-void render_scene(t_vars *vars, t_sphere *sphere, t_plane *plane, t_light *light)
-{
-    int y = 0;
-    int x;
-    while (y < HEIGHT) {
-        x = 0;
-        while (x < WIDTH) {
-            t_ray ray = {
-                .origin = {0, 0, 0},
-                .direction = normalize((t_vector){
-                    x - WIDTH / 2,
-                    y - HEIGHT / 2,
-                    -WIDTH / (2 * tan(M_PI / 6))
-                })
-            };
-            
-            float t_sphere_value = intersect_ray_sphere(ray, *sphere);
-            float t_plane_value = intersect_ray_plane(ray, *plane);
-            int color = create_trgb(0, 0, 0, 255);
-
-            if (t_sphere_value < INFINITY && (t_sphere_value < t_plane_value || t_plane_value == INFINITY)) {
-                t_vector intersection_point = vector_add(ray.origin, vector_scale(ray.direction, t_sphere_value));
-                t_vector normal = normalize(vector_sub(intersection_point, sphere->center));
-                t_vector light_dir = normalize(vector_sub(light->position, intersection_point));
-                double diff = fmax(dot(normal, light_dir), 0.0) * light->intensity;
-                color = adjust_color_by_light(sphere->color, diff);
-            } else if (t_plane_value != INFINITY) {
-                color = create_trgb(0, plane->color[0], plane->color[1], plane->color[2]);
-            }
-
-            my_mlx_pixel_put(vars, x, y, color);
-            x++;
-        }
-        y++;
-    }
-}
-*/
-/*
-int key_hook(int keycode, t_vars *vars) 
-{
-    if (keycode == 'w') {
-        g_sphere.diameter = fmax(1, g_sphere.diameter + 1);
-    } else if (keycode == 's') {
-        g_sphere.diameter = fmin(100, g_sphere.diameter - 1);
-    }
-
-    render_scene(vars, &g_sphere, &g_plane, &g_light);
-    mlx_put_image_to_window(vars->mlx, vars->win, vars->img.img_ptr, 0, 0);
-    return 0;
-}
-*/
-
-/*
-int main(void)
-{
-    t_vars vars;
-    g_sphere = (t_sphere){
-        .center = {0, 0, -20},
-        .diameter = 10.0,
-        .color = {255, 0, 0}
-    };
-
-    g_plane = (t_plane){
-        .point = {0.0, -5.0, 0.0},
-        .normal = {0.0, 1.0, 0.0},
-        .color = {0, 255, 0}
-    };
-
-    g_light = (t_light){
-        .position = {-20.0, 20.0, 20.0},
-        .intensity = 0.7,
-        .color = {255, 255, 255}
-    };
-
-     g_cylinder = (t_cylinder){
-        .center = {2.0, 0.0, -15.0}, // Example position
-        .direction = {0.0, 1.0, 0.0}, // Example orientation (aligned with Y axis)
-        .diameter = 3.0,
-        .height = 10.0,
-        .color = {0, 0, 255} // Blue
-    };
-
-    vars.camera.fov = 90;
-    
-    vars.mlx = mlx_init();
-    vars.win = mlx_new_window(vars.mlx, WIDTH, HEIGHT, "MiniRT - bitch");
-    vars.img.img_ptr = mlx_new_image(vars.mlx, WIDTH, HEIGHT);
-    vars.img.addr = mlx_get_data_addr(vars.img.img_ptr, &vars.img.bits_per_pixel, &vars.img.line_length, &vars.img.endian);
-    //mlx_key_hook(vars.win, key_hook, &vars);
-    render_cylinder_basic(&vars, g_cylinder, g_light);
-    mlx_put_image_to_window(vars.mlx, vars.win, vars.img.img_ptr, 0, 0);
-    mlx_loop(vars.mlx);
-
-    return 0;
-}
-
-*/
 
 t_color color(float red, float green, float blue)
 {
@@ -736,14 +634,14 @@ t_ray transform(t_ray ray, t_matrix m)
     transformed_ray.direction = multiply_matrix_tuple(m, ray.direction);
     return transformed_ray;
 }
-
+/*
 void render_sphere(t_vars *vars) {
     int x, y;
     int canvas_pixel = 500;
-    double wall_size = -1.0;
+    double wall_size = 0.3;
     t_sphere s = sphere();
     double pixel_size = wall_size / canvas_pixel;
-    double wall_z = -3;
+    double wall_z = -4.5;
     double half = wall_size / 2.0;
     int color = 0x00FF0000; 
 
@@ -779,3 +677,5 @@ int main() {
     mlx_loop(vars.mlx);
     return (0);
 }
+
+*/
