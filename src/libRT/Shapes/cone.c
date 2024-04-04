4e24c6d7c28259fcd77efb415d5b59b7817f9d04
Alessio Benincasa <albeninc@bess-f1r4s10.clusters.42paris.fr>
2024-04-04 17:17:41 +0200

    norme almost fixed

--- src/libRT/Shapes/cone.c
+++ src/libRT/Shapes/cone.c
@@ -3,127 +3,163 @@
 /*                                                        :::      ::::::::   */
 /*   cone.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svolodin <svolodin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: albeninc <albeninc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 14:27:48 by svolodin          #+#    #+#             */
-/*   Updated: 2024/04/03 14:03:34 by svolodin         ###   ########.fr       */
+/*   Updated: 2024/04/04 17:16:52 by albeninc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "all.h"
 
-t_cone cone(void)
+t_cone	cone(void)
 {
-    t_cone cone;
-    cone.transform = identity_matrix();
-    cone.minimum = 0;
-    cone.maximum = 3;
-    cone.closed = 1;
-    cone.inverted = 1;
-    cone.material = material();
+	t_cone	cone;
+
+	cone.transform = identity_matrix();
+	cone.minimum = 0;
+	cone.maximum = 3;
+	cone.closed = 1;
+	cone.inverted = 1;
+	cone.material = material();
 	cone.material.specular = 0;
-    cone.material.diffuse = 0.6;
-    cone.material.shininess = 50;
-    return cone;
+	cone.material.diffuse = 0.6;
+	cone.material.shininess = 50;
+	return (cone);
 }
-t_tuple local_normal_at_cone(t_cone cone, t_tuple point)
+
+t_tuple	local_normal_at_cone(t_cone cone, t_tuple point)
 {
-    double radius = cone.maximum / 2;
-    double dist = pow(point.x, 2) + pow(point.z, 2);
-    if (dist < pow(cone.maximum, 2) && point.y >= cone.maximum - EPSILON)
-        return vector(0, 1, 0);
-    else if (dist < pow(cone.maximum, 2) && point.y <= cone.minimum + EPSILON)
-        return vector(0, -1, 0);
-    else
-    {
-        double slantHeight = sqrt(pow(point.x, 2) + pow(point.z, 2));
-        double height = fabs(cone.maximum - cone.minimum);
-        double slope = radius / height;
-        double y = slope * slantHeight;
-        if (cone.inverted)
-            y = -y;
-        return (vector(point.x, y, point.z));
-    }
+	double	radius;
+	double	dist;
+	double	slant_height;
+	double	height;
+	double	slope;
+	double	y;
+
+	radius = cone.maximum / 2;
+	dist = pow(point.x, 2) + pow(point.z, 2);
+	if (dist < pow(cone.maximum, 2) && point.y >= cone.maximum - EPSILON)
+		return (vector(0, 1, 0));
+	else if (dist < pow(cone.maximum, 2) && point.y <= cone.minimum + EPSILON)
+		return (vector(0, -1, 0));
+	else
+	{
+		slant_height = sqrt(pow(point.x, 2) + pow(point.z, 2));
+		height = fabs(cone.maximum - cone.minimum);
+		slope = radius / height;
+		y = slope * slant_height;
+		if (cone.inverted)
+			y = -y;
+		return (vector(point.x, y, point.z));
+	}
 }
 
 t_tuple normal_at_cone(t_cone cone, t_tuple p)
 {
     t_tuple object_point = multiply_matrix_tuple(inverse(cone.transform), p);
-    t_tuple object_normal = local_normal_at_cone(cone, object_point);
-    t_tuple world_normal = multiply_matrix_tuple(transpose_matrix(inverse(cone.transform)), object_normal);
+    t_tuple object = local_normal_at_cone(cone, object_point);
+    t_matrix inverse_transform = inverse(cone.transform);
+    t_matrix transposed = transpose_matrix(inverse_transform);
+    t_tuple world_normal = multiply_matrix_tuple(transposed, object);
     world_normal.w = 0;
 
     return (normalize(world_normal));
 }
 
-int check_cap_cone(t_ray ray, double t, t_cone *cone, int is_lower)
+int	check_cap_cone(t_ray ray, double t, t_cone *cone, int is_lower)
 {
-    double x = ray.origin.x + t * ray.direction.x;
-    double z = ray.origin.z + t * ray.direction.z;
-    double cap = is_lower ? cone->minimum : cone->maximum;
-    return (x * x + z * z) <= (cap * cap);
+	double	x;
+	double	z;
+	double	cap;
+
+	x = ray.origin.x + t * ray.direction.x;
+	z = ray.origin.z + t * ray.direction.z;
+	if (is_lower)
+		cap = cone->minimum;
+	else
+		cap = cone->maximum;
+	if ((x * x + z * z) <= (cap * cap))
+		return (1);
+	else
+		return (0);
 }
 
-void add_intersection_cone(t_intersections *xs, double t, t_cone *cone)
+void	add_intersection_cone(t_intersections *xs, double t, t_cone *cone)
 {
-    xs->count++;
-    xs->intersections = realloc(xs->intersections, xs->count * sizeof(t_intersection));
-    xs->intersections[xs->count - 1].t = t;
-    xs->intersections[xs->count - 1].object.cone = cone;
-    xs->intersections[xs->count - 1].type = CONE;
+	xs->count++;
+	xs->intersections = realloc(xs->intersections, xs->count
+			* sizeof(t_intersection));
+	xs->intersections[xs->count - 1].t = t;
+	xs->intersections[xs->count - 1].object.cone = cone;
+	xs->intersections[xs->count - 1].type = CONE;
 }
 
-t_intersections intersect_cone(t_cone *cone, t_ray r)
+static t_intersections	intersect_cone_sides(t_cone *cone,
+		t_ray transformed_ray)
 {
-    t_intersections xs;
-    xs.count = 0;
-    xs.intersections = NULL;
+	t_intersections	xs;
+	double			sqrt_disc;
+	double			a;
+	double			b;
+	double			c;
+	double			disc;
+	double			t0;
+	double			t1;
+	double			temp;
+	double			y0;
+	double			y1;
 
-	t_ray ray = transform(r, inverse(cone->transform));
-
-    double a = pow(ray.direction.x, 2) - pow(ray.direction.y, 2) + pow(ray.direction.z, 2);
-    double b = 2 * ray.origin.x * ray.direction.x - 2 * ray.origin.y * ray.direction.y + 2 * ray.origin.z *ray.direction.z;
-    double c = pow(ray.origin.x, 2) - pow(ray.origin.y, 2) + pow(ray.origin.z, 2);
-    double disc = b * b - 4 * a * c;
-    
-    if (fabs(a) < EPSILON && fabs(b) < EPSILON)
-        return xs;
-        
-    if (disc >= 0)
-    {
-        double t0 = (-b - sqrt(disc)) / (2*a);
-        double t1 = (-b + sqrt(disc)) / (2*a);
-        if (t0 > t1) { double temp = t0; t0 = t1; t1 = temp; }
-
-        double y0 = ray.origin.y + t0 * ray.direction.y;
-        if (y0 > cone->minimum && y0 < cone->maximum)
-            add_intersection_cone(&xs, t0, cone);
-
-        double y1 = ray.origin.y + t1 * ray.direction.y;
-        if (y1 > cone->minimum && y1 < cone->maximum)
-            add_intersection_cone(&xs, t1, cone);
-    }
-
-
-    if (cone->closed && fabs(ray.direction.y) > EPSILON)
-    {
-        double t_lower = (cone->minimum - ray.origin.y) / ray.direction.y;
-        if (check_cap_cone(ray, t_lower, cone, 1))
-            add_intersection_cone(&xs, t_lower, cone);
-        double t_upper = (cone->maximum - ray.origin.y) / ray.direction.y;
-        if (check_cap_cone(ray, t_upper, cone, 0))
-            add_intersection_cone(&xs, t_upper, cone);
-    }
-
-    if (fabs(a) < EPSILON && fabs(b) >= EPSILON)
-    {
-        double t = -c / (2 * b);
-        double y = ray.origin.y + t * ray.direction.y;
-        if (cone->minimum < y && y < cone->maximum) {
-            add_intersection_cone(&xs, t, cone);
-        }
-    }
-
-    return xs;
+	xs.count = 0;
+	xs.intersections = NULL;
+	a = pow(transformed_ray.direction.x, 2) - pow(transformed_ray.direction.y,
+			2) + pow(transformed_ray.direction.z, 2);
+	b = 2 * transformed_ray.origin.x * transformed_ray.direction.x - 2
+		* transformed_ray.origin.y * transformed_ray.direction.y + 2
+		* transformed_ray.origin.z * transformed_ray.direction.z;
+	c = pow(transformed_ray.origin.x, 2) - pow(transformed_ray.origin.y, 2)
+		+ pow(transformed_ray.origin.z, 2);
+	disc = b * b - 4 * a * c;
+	if (fabs(a) >= EPSILON && disc >= 0)
+	{
+		sqrt_disc = sqrt(disc);
+		t0 = (-b - sqrt_disc) / (2 * a);
+		t1 = (-b + sqrt_disc) / (2 * a);
+		if (t0 > t1)
+		{
+			temp = t0;
+			t0 = t1;
+			t1 = temp;
+		}
+		y0 = transformed_ray.origin.y + t0 * transformed_ray.direction.y;
+		y1 = transformed_ray.origin.y + t1 * transformed_ray.direction.y;
+		if (y0 > cone->minimum && y0 < cone->maximum)
+			add_intersection_cone(&xs, t0, cone);
+		if (y1 > cone->minimum && y1 < cone->maximum)
+			add_intersection_cone(&xs, t1, cone);
+	}
+	return (xs);
 }
 
+t_intersections	intersect_cone(t_cone *cone, t_ray r)
+{
+	t_intersections	xs;
+	t_ray			transformed_ray;
+	double			t_lower;
+	double			t_upper;
+
+	transformed_ray = transform(r, inverse(cone->transform));
+	xs = intersect_cone_sides(cone, transformed_ray);
+	if (cone->closed && fabs(transformed_ray.direction.y) > EPSILON)
+	{
+		t_lower = (cone->minimum - transformed_ray.origin.y)
+			/ transformed_ray.direction.y;
+		if (check_cap_cone(transformed_ray, t_lower, cone, 1))
+			add_intersection_cone(&xs, t_lower, cone);
+		t_upper = (cone->maximum - transformed_ray.origin.y)
+			/ transformed_ray.direction.y;
+		if (check_cap_cone(transformed_ray, t_upper, cone, 0))
+			add_intersection_cone(&xs, t_upper, cone);
+	}
+	return (xs);
+}
--- src/libRT/Shapes/cylinder.c
+++ src/libRT/Shapes/cylinder.c
@@ -3,116 +3,173 @@
 /*                                                        :::      ::::::::   */
 /*   cylinder.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svolodin <svolodin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: albeninc <albeninc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 13:32:48 by albeninc          #+#    #+#             */
-/*   Updated: 2024/04/03 14:03:34 by svolodin         ###   ########.fr       */
+/*   Updated: 2024/04/04 16:09:56 by albeninc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "all.h"
 
-t_intersections intersect_cylinder(t_cylinder *cyl, t_ray ray)
+typedef struct s_coef
 {
-    t_intersections xs;
-    xs.count = 0;
-    xs.intersections = NULL;
+	double				a;
+	double				b;
+	double				c;
+	double				disc;
+	double				t1;
+	double				t2;
+}						t_coef;
 
-    double radius = cyl->diameter / 2;
-    t_ray tr_ray = transform(ray, inverse(cyl->transform));
-    
-    double a = pow(tr_ray.direction.x, 2) + pow(tr_ray.direction.z, 2);
-    if (fabs(a) > EPSILON) {
-        double b = 2 * tr_ray.origin.x * tr_ray.direction.x + 2 * tr_ray.origin.z * tr_ray.direction.z;
-        double c = pow(tr_ray.origin.x, 2) + pow(tr_ray.origin.z, 2) - pow(radius, 2);
-        double disc = b * b - 4 * a * c;
+static t_coef	calculate_quadratic_coefficients(t_cylinder *cyl, t_ray tr_ray)
+{
+	double	radius;
+	t_coef	coef;
 
-        if (disc >= 0) {
-            double sqrt_disc = sqrt(disc);
-            double t0 = (-b - sqrt_disc) / (2 * a);
-            double t1 = (-b + sqrt_disc) / (2 * a);
-
-            if (t0 > t1) {
-                double temp = t0;
-                t0 = t1;
-                t1 = temp;
-            }
-
-            double y0 = tr_ray.origin.y + t0 * tr_ray.direction.y;
-            if (y0 > cyl->minimum && y0 < cyl->maximum) {
-                add_intersection_cylinder(&xs, t0, cyl);
-            }
-            double y1 = tr_ray.origin.y + t1 * tr_ray.direction.y;
-            if (y1 > cyl->minimum && y1 < cyl->maximum) {
-                add_intersection_cylinder(&xs, t1, cyl);
-            }
-        }
-    }
-
-    if (cyl->closed && fabs(tr_ray.direction.y) > EPSILON) {
-        double t_lower = (cyl->minimum - tr_ray.origin.y) / tr_ray.direction.y;
-        if (check_cap(tr_ray, t_lower, radius)) {
-            add_intersection_cylinder(&xs, t_lower, cyl);
-        }
-
-        double t_upper = (cyl->maximum - tr_ray.origin.y) / tr_ray.direction.y;
-        if (check_cap(tr_ray, t_upper, radius)) {
-            add_intersection_cylinder(&xs, t_upper, cyl);
-        }
-    }
-
-    return xs;
+	radius = cyl->diameter / 2;
+	coef.a = pow(tr_ray.direction.x, 2) + pow(tr_ray.direction.z, 2);
+	coef.b = 2 * tr_ray.origin.x * tr_ray.direction.x + 2 * tr_ray.origin.z
+		* tr_ray.direction.z;
+	coef.c = pow(tr_ray.origin.x, 2) + pow(tr_ray.origin.z, 2) - pow(radius, 2);
+	return (coef);
 }
 
-int check_cap(t_ray ray, double t, double radius)
+static t_coef	solve_cylinder_quadratic(t_cylinder *cyl, t_ray tr_ray)
 {
-    double x = ray.origin.x + t * ray.direction.x;
-    double z = ray.origin.z + t * ray.direction.z;
-    return (x*x + z*z) <= (radius * radius);
+	t_coef	coef;
+	t_coef	result;
+	double	disc;
+	double	sqrt_disc;
+	double	temp;
+
+	coef = calculate_quadratic_coefficients(cyl, tr_ray);
+	disc = coef.b * coef.b - 4 * coef.a * coef.c;
+	result.disc = disc;
+	if (disc >= 0)
+	{
+		sqrt_disc = sqrt(disc);
+		result.t1 = (-coef.b - sqrt_disc) / (2 * coef.a);
+		result.t2 = (-coef.b + sqrt_disc) / (2 * coef.a);
+		if (result.t1 > result.t2)
+		{
+			temp = result.t1;
+			result.t1 = result.t2;
+			result.t2 = temp;
+		}
+	}
+	return (result);
 }
 
-t_cylinder cylinder(void)
+static t_intersections	intersect_cylinder_sides(t_cylinder *cyl, t_ray tr_ray)
 {
-    t_cylinder cyl;
-    cyl.transform = identity_matrix();
-    cyl.minimum = 0;
-    cyl.maximum = 3;
-    cyl.closed = 1;
-    cyl.material = material();
-    cyl.material.specular = 0;
-    cyl.material.diffuse = 0.6;
-    cyl.material.shininess = 50;
-    return cyl;
+	t_intersections	xs;
+	t_coef			coef;
+	double			y0;
+	double			y1;
+
+	xs.count = 0;
+	xs.intersections = NULL;
+	coef = solve_cylinder_quadratic(cyl, tr_ray);
+	if (coef.disc >= 0)
+	{
+		y0 = tr_ray.origin.y + coef.t1 * tr_ray.direction.y;
+		if (y0 > cyl->minimum && y0 < cyl->maximum)
+			add_intersection_cylinder(&xs, coef.t1, cyl);
+		y1 = tr_ray.origin.y + coef.t2 * tr_ray.direction.y;
+		if (coef.t1 != coef.t2 && y1 > cyl->minimum && y1 < cyl->maximum)
+			add_intersection_cylinder(&xs, coef.t2, cyl);
+	}
+	return (xs);
 }
 
-t_tuple local_normal_at_cylinder(t_cylinder cyl, t_tuple point)
+t_intersections	intersect_cylinder(t_cylinder *cyl, t_ray ray)
 {
-    double radius = cyl.diameter / 2;
-    double dist = pow(point.x, 2) + pow(point.z, 2);
-    if (dist < pow(radius, 2) && point.y >= cyl.maximum - EPSILON)
-        return vector(0, 1, 0);
-    else if (dist < pow(radius, 2) && point.y <= cyl.minimum + EPSILON)
-        return vector(0, -1, 0);
-    else
-        return vector(point.x, 0, point.z);
+	t_intersections	xs;
+	double			radius;
+	t_ray			tr_ray;
+	double			t_lower;
+	double			t_upper;
+
+	xs = intersect_cylinder_sides(cyl, transform(ray, inverse(cyl->transform)));
+	if (cyl->closed && fabs(ray.direction.y) > EPSILON)
+	{
+		radius = cyl->diameter / 2;
+		tr_ray = transform(ray, inverse(cyl->transform));
+		t_lower = (cyl->minimum - tr_ray.origin.y) / tr_ray.direction.y;
+		if (check_cap(tr_ray, t_lower, radius))
+			add_intersection_cylinder(&xs, t_lower, cyl);
+		t_upper = (cyl->maximum - tr_ray.origin.y) / tr_ray.direction.y;
+		if (check_cap(tr_ray, t_upper, radius))
+			add_intersection_cylinder(&xs, t_upper, cyl);
+	}
+	return (xs);
 }
 
-t_tuple normal_at_cylinder(t_cylinder cylinder, t_tuple p)
+int	check_cap(t_ray ray, double t, double radius)
 {
-    t_tuple object_point = multiply_matrix_tuple(inverse(cylinder.transform), p);
-    t_tuple object_normal = local_normal_at_cylinder(cylinder, object_point);
-    t_tuple world_normal = multiply_matrix_tuple(transpose_matrix(inverse(cylinder.transform)), object_normal);
-    world_normal.w = 0;
+	double	x;
+	double	z;
 
-    return (normalize(world_normal));
+	x = ray.origin.x + t * ray.direction.x;
+	z = ray.origin.z + t * ray.direction.z;
+	return ((x * x + z * z) <= (radius * radius));
 }
 
-
-void add_intersection_cylinder(t_intersections *xs, double t, t_cylinder *cyl)
+t_cylinder	cylinder(void)
 {
-    xs->count++;
-    xs->intersections = realloc(xs->intersections, xs->count * sizeof(t_intersection));
-    xs->intersections[xs->count - 1].t = t;
-    xs->intersections[xs->count - 1].object.cylinder = cyl;
-    xs->intersections[xs->count - 1].type = CYLINDER;
+	t_cylinder	cyl;
+
+	cyl.transform = identity_matrix();
+	cyl.minimum = 0;
+	cyl.maximum = 3;
+	cyl.closed = 1;
+	cyl.material = material();
+	cyl.material.specular = 0;
+	cyl.material.diffuse = 0.6;
+	cyl.material.shininess = 50;
+	return (cyl);
+}
+
+t_tuple	local_normal_at_cylinder(t_cylinder cyl, t_tuple point)
+{
+	double	radius;
+	double	dist;
+
+	radius = cyl.diameter / 2;
+	dist = pow(point.x, 2) + pow(point.z, 2);
+	if (dist < pow(radius, 2) && point.y >= cyl.maximum - EPSILON)
+		return (vector(0, 1, 0));
+	else if (dist < pow(radius, 2) && point.y <= cyl.minimum + EPSILON)
+		return (vector(0, -1, 0));
+	else
+		return (vector(point.x, 0, point.z));
+}
+
+t_tuple	normal_at_cylinder(t_cylinder cylinder, t_tuple p)
+{
+	t_tuple		object_point;
+	t_tuple		object_normal;
+	t_tuple		world_normal;
+	t_matrix	inverse_transform;
+	t_matrix	transposed_inverse;
+
+	object_point = multiply_matrix_tuple(inverse(cylinder.transform), p);
+	object_normal = local_normal_at_cylinder(cylinder, object_point);
+	inverse_transform = inverse(cylinder.transform);
+	transposed_inverse = transpose_matrix(inverse_transform);
+	world_normal = multiply_matrix_tuple(transposed_inverse, object_normal);
+	world_normal.w = 0;
+	return (normalize(world_normal));
+}
+
+void	add_intersection_cylinder(t_intersections *xs, double t,
+		t_cylinder *cyl)
+{
+	xs->count++;
+	xs->intersections = realloc(xs->intersections, xs->count
+			* sizeof(t_intersection));
+	xs->intersections[xs->count - 1].t = t;
+	xs->intersections[xs->count - 1].object.cylinder = cyl;
+	xs->intersections[xs->count - 1].type = CYLINDER;
 }
