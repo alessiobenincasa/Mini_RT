6d47f9a38450cb7d042b1532c4ae21e2bf492942
Alessio Benincasa <albeninc@paul-f5Ar3s3.clusters.42paris.fr>
2024-04-05 19:36:43 +0200

    fixed the norme for cone except the exces of arguments

--- src/libRT/Shapes/cone_inter.c
+++ src/libRT/Shapes/cone_inter.c
@@ -6,7 +6,7 @@
 /*   By: albeninc <albeninc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 19:09:47 by albeninc          #+#    #+#             */
-/*   Updated: 2024/04/05 19:30:26 by albeninc         ###   ########.fr       */
+/*   Updated: 2024/04/05 19:34:22 by albeninc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,6 @@ typedef struct s_coef
 	double		b;
 	double		c;
 }				t_coef;
-
-t_cone	cone(void)
-{
-	t_cone	cone;
-
-	cone.transform = identity_matrix();
-	cone.minimum = 0;
-	cone.maximum = 3;
-	cone.closed = 1;
-	cone.inverted = 1;
-	cone.material = material();
-	cone.material.specular = 0;
-	cone.material.diffuse = 0.6;
-	cone.material.shininess = 50;
-	return (cone);
-}
-
-void	calculate_coefficients(t_ray ray, double *a, double *b, double *c)
-{
-	*a = pow(ray.direction.x, 2) - pow(ray.direction.y, 2)
-		+ pow(ray.direction.z, 2);
-	*b = 2 * ray.origin.x * ray.direction.x - 2 * ray.origin.y * ray.direction.y
-		+ 2 * ray.origin.z * ray.direction.z;
-	*c = pow(ray.origin.x, 2) - pow(ray.origin.y, 2) + pow(ray.origin.z, 2);
-}
 
 void	calculate_and_sort_roots(double a, double b, double c, double *t0,
 		double *t1)
