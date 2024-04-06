5d13dca713d7df819e563a843bb90ed7297f975e
Alessio Benincasa <albeninc@paul-f4Br1s2.clusters.42paris.fr>
2024-04-06 07:57:45 +0200

    leaks

--- src/hooks.c
+++ src/hooks.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   hooks.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svolodin <svolodin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: albeninc <albeninc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 13:28:39 by svolodin          #+#    #+#             */
-/*   Updated: 2024/04/05 13:28:50 by svolodin         ###   ########.fr       */
+/*   Updated: 2024/04/06 07:31:43 by albeninc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "all.h"
 
+t_matrix_registry	g_matrix_registry;
 int	handle_close(t_vars *vars)
 {
 	if (vars->img.img_ptr)
@@ -27,13 +28,18 @@ int	handle_close(t_vars *vars)
 		free(vars->mlx);
 	}
 	vars->mlx = NULL;
-    free_scene_data(vars->scene);
+	free_scene_data(vars->scene);
+	for (size_t i = 0; i < g_matrix_registry.count; i++)
+	{
+		free(g_matrix_registry.matrices[i].elements);
+	}
+	free(g_matrix_registry.matrices);
 	exit(EXIT_SUCCESS);
 }
 
-int key_hook(int keycode, t_vars *vars)
+int	key_hook(int keycode, t_vars *vars)
 {
-    if (keycode == XK_Escape)
-        handle_close(vars);
-    return (0);
+	if (keycode == XK_Escape)
+		handle_close(vars);
+	return (0);
 }
