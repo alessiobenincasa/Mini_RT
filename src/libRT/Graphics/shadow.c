bfb698adbfab5efb59740761f4c05a7de501096c
Alessio Benincasa <albeninc@paul-f4Br1s2.clusters.42paris.fr>
2024-04-06 08:15:04 +0200

    fixed more leaks

--- src/libRT/Graphics/shadow.c
+++ src/libRT/Graphics/shadow.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   shadow.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svolodin <svolodin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: albeninc <albeninc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 13:11:49 by albeninc          #+#    #+#             */
-/*   Updated: 2024/04/04 10:55:04 by svolodin         ###   ########.fr       */
+/*   Updated: 2024/04/06 08:13:59 by albeninc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,6 @@ int	is_shadowed(t_world world, t_tuple point, t_tuple light_pos)
 	// 	distance, (h != NULL ? h->t : -1), (h != NULL && h->t < distance) ? 1 : 0);
 	if (h != NULL && h->t < distance)
 		return (1);
+	free(inters.intersections);
 	return (0);
 }
